ad86dff52f5dd0fab0d0aa84d218c11728e5e0cd
Ed: step <redvepr@gmail.com>
2017-11-16 16:07:10 +0200

    fix cache; set out

--- sc.sh
+++ sc.sh
@@ -1,13 +1,22 @@
 LAN="192.168.1."
 log="ip.log"
+if [ -f $log ]
+then
+ out=( $(cat $log) )
+else
+ eval "out=(`echo [{0..254}]=2`)"
+fi
 
 for i in {117..118}
 do
 
+if [ ${out[$i]} = 2 ]
+then
 old[$i]=${out[$i]}
 ping -c 1 ${LAN}${i} >> /dev/null
 out[$i]=$?
 [ ${old[$i]} = ${out[$i]} ] && unset old[$i]
+fi
 done
 
 if [ ${#old[@]} -gt 0 ]
