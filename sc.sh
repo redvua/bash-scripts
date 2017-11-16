cb9c2ce515c0b2b46bc2454ff5085e2b402da7c3
Ed: step <redvepr@gmail.com>
2017-11-16 16:12:45 +0200

    add invalidate cache

--- sc.sh
+++ sc.sh
@@ -2,6 +2,8 @@ LAN="192.168.1."
 log="ip.log"
 if [ -f $log ]
 then
+ let tlc="`date +%s` - `stat -c%Y $log`"
+ [ 180 -le $tlc ] && invalidate=true
  out=( $(cat $log) )
 else
  eval "out=(`echo [{0..254}]=2`)"
@@ -10,7 +12,7 @@ fi
 for i in {117..118}
 do
 
-if [ ${out[$i]} = 2 ]
+if [ ${out[$i]} = 2 -o "$invalidate" = true ]
 then
 old[$i]=${out[$i]}
 ping -c 1 ${LAN}${i} >> /dev/null
@@ -24,6 +26,7 @@ then
  for i in ${!old[@]} ; do
   echo "${LAN}${i} ${old[$i]} -> ${out[$i]}"
  done
+ invalidate=true
 fi
 
-echo ${out[@]} > $log
+[ "$invalidate" = true ] && echo ${out[@]} > $log
