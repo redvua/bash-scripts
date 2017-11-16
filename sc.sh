42050733c49de41c0bd6c88ca3e3b54d027461ac
Ed: step <redvepr@gmail.com>
2017-11-16 16:25:48 +0200

    fix

--- sc.sh
+++ sc.sh
@@ -1,23 +1,23 @@
 LAN="192.168.1."
-log="ip.log"
+log="/home/ip.log" # cache file name
+
 if [ -f $log ]
 then
- let tlc="`date +%s` - `stat -c%Y $log`"
+ let tlc="`date +%s` - `stat -c%Y $log`" # time life cache file in sec
  [ 180 -le $tlc ] && invalidate=true
  out=( $(cat $log) )
 else
  eval "out=(`echo [{0..254}]=2`)"
 fi
 
-for i in {117..118}
+for i in {117..119}
 do
-
 if [ ${out[$i]} = 2 -o "$invalidate" = true ]
 then
-old[$i]=${out[$i]}
-ping -c 1 ${LAN}${i} >> /dev/null
-out[$i]=$?
-[ ${old[$i]} = ${out[$i]} ] && unset old[$i]
+ old[$i]=${out[$i]}
+ ping -c 1 ${LAN}${i} >> /dev/null
+ out[$i]=$?
+ [ ${old[$i]} = ${out[$i]} ] && unset old[$i]
 fi
 done
 
